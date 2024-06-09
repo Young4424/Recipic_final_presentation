4937c28a0c7a7ec76c49e6a548e0e7071f6a7e9a
장은서 <124859156+megjangg@users.noreply.github.com>
2024-06-09 23:57:24 +0900

    Update recipes.dart
    UPDATE RECIPE

--- lib/data/recipes.dart
+++ lib/data/recipes.dart
@@ -28,5 +28,202 @@ List<Map<String, dynamic>> recipes = [
   },
 
 
+      {
+        'title': '상큼하고 고소한 레몬바',
+        'description': '레몬바',
+        'time': '60',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/07/30/48b9375185fc71df43172a70537db33c.jpg',
+        'link': 'https://www.10000recipe.com/recipe/1940311',
+        'ingredients': ['버터', '슈가파우더', '소금', '박력분', '달걀', '설탕', '레몬즙', '레몬쥬스', '레몬 제스트']
+    },
+    {
+        'title': '레몬차만드는법 레몬꿀절임',
+        'description': '레몬차',
+        'time': '',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/15/9032a7ef9d598dd0ab1079fa48319d001.jpg',
+        'link': 'https://www.10000recipe.com/recipe/4672651',
+        'ingredients': ['레몬', '백설탕', '꿀(조청)']
+    },
+    {
+        'title': '쉽게 만드는 레몬커드(레몬잼)',
+        'description': '레몬커드',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/02/24e4e2b20cd8b5ded69139c5699fb83b1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/5419429',
+        'ingredients': ['레몬', '레몬제스트', '계란', '설탕', '버터', '소금']
+    },
+    {
+        'title': '레몬청',
+        'description': '레몬청',
+        'time': '60',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/11/11/2ccba0a69066ef3c2dc3d05dad9272231.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6834349',
+        'ingredients': ['레몬', '설탕']
+    },
+    {
+        'title': '스페인의 맛을 집에서 느껴보자! 상그리아',
+        'description': '상그리아',
+        'time': '10',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2017/07/06/470c2a7b8148e7a2cf0b992f344b4a341.png',
+        'link': 'https://www.10000recipe.com/recipe/6872318',
+        'ingredients': ['레드와인', '오렌지', '레몬', '사과', '사이다', '얼음']
+    },
+    {
+        'title': '소주로 만든 깻잎 모히토',
+        'description': '깻잎모히토',
+        'time': '10',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/04/de304f65effc6a8ae764db10f745346c1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6748228',
+        'ingredients': ['레몬', '설탕', '사이다', '소주', '깻잎', '얼음', '소금']
+    },
+    {
+        'title': '한그릇~~마늘토마토볶음밥만들기',
+        'description': '마늘토마토볶음밥',
+        'time': '15',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/10/26/de09acdcf5ad2b12403a2179cba392d51.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6836868',
+        'ingredients': ['마늘', '방울토마토', '밥', '소금', '굴소스', '파슬리가루', '기름']
+    },
+    {
+        'title': '건강한 수제버거! 미니토마토치즈버거',
+        'description': '미니토마토치즈버거',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2017/07/14/a019a6307abe53c6d7280260ad464cd61.png',
+        'link': 'https://www.10000recipe.com/recipe/6872737',
+        'ingredients': ['모닝빵', '돼지고기 다짐육', '소고기 다짐육', '양파', '빵가루', '양상추', '체다 치즈', '토마토', '버터', '소금', '후추', '마요네즈', '케찹']
+    },
+    {
+        'title': '새우 토마토 소스 리조또',
+        'description': '새우리조또',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/08/12/bf995e4c099c5f8c08445c22e74b49f01.jpg',
+        'link': 'https://www.10000recipe.com/recipe/1254054',
+        'ingredients': ['밥', '양파', '새우', '슬라이스 치즈', '쪽파', '시판 스파게티 소스', '우유', '버터']
+    },
+    {
+        'title': '상큼한 사과차',
+        'description': '사과차',
+        'time': '15',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/06/08/9ae69b64678a95658e2a1ed3221a3ff5.jpg',
+        'link': 'https://www.10000recipe.com/recipe/3007642',
+        'ingredients': ['사과', '레몬', '꿀']
+    },
+    {
+        'title': '레몬청만들기 수제 레몬청!!',
+        'description': '레몬청',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2017/05/18/86689ac530d2d94d1c6846aa2392d70e1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6869989',
+        'ingredients': ['레몬', '설탕', '베이킹소다', '식초']
+    }
+
+    {
+        'title': '고급진 맛의 사과스프',
+        'description': '사과스프',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/10/04/6862a743ecf487aa7be38316b053d4ff1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6835531',
+        'ingredients': ['사과', '양파', '우유', '생크림', '계피가루', '파슬리', '소금']
+    },
+    {
+        'title': '사과단자 만들기',
+        'description': '사과단자',
+        'time': '90',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/01/89762ce70377aed11a67a6e5ed52aaf91.jpg',
+        'link': 'https://www.10000recipe.com/recipe/4142873',
+        'ingredients': ['사과', '찹쌀가루', '딸기가루', '코코넛가루', '장식용대추', '호박씨', '설탕', '계피가루']
+    },
+    {
+        'title': '식빵사과파이 애플파이 만드는 법',
+        'description': '식빵사과파이',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2017/05/18/e46fb22a64297a656310e57012ae59d81.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6869948',
+        'ingredients': ['사과', '버터', '설탕', '식빵', '올리고당', '계피가루', '계란']
+    },
+    {
+        'title': '쉽고 맛있는 영국 디저트 애플 크럼블',
+        'description': '애플크럼블',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/11/20/bc84a648a87544e388fff6fe80f8afd81.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6839355',
+        'ingredients': ['사과', '설탕', '바닐라 에센스', '계피 가루', '무염 버터', '밀가루']
+    },
+    {
+        'title': '사과포켓쿠키 만들기',
+        'description': '사과포켓쿠키',
+        'time': '90',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2017/01/19/9b732c4cdbd06e3ab298b56f7aad047f1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6864078',
+        'ingredients': ['사과', '설탕', '계피가루', '무염버터', '설탕', '노른자', '바닐라익스트랙', '박력분', '아몬드가루']
+    },
+    {
+        'title': '<오븐사과칩 & 사과차> 비타민 드세요:D',
+        'description': '사과칩',
+        'time': '120',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/23/3c1a52acd924fefd99838be3b67192d81.jpg',
+        'link': 'https://www.10000recipe.com/recipe/3147580',
+        'ingredients': ['사과', '설탕']
+    },
+    {
+        'title': '가지토마토 스파게티',
+        'description': '토마토스파게티',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2018/07/27/b5c1f7b47d10bfc54ba9f739832355601.jpg',
+        'link': 'https://www.10000recipe.com/recipe/1997142',
+        'ingredients': ['가지', '스파게티면', '바질가루', '파슬리가루', '파마산치즈', '토마토', '베이컨', '마늘', '양파', '간장', '소금', '후추']
+    },
+    {
+        'title': '다이어트식 ; 계란 토마토 볶음',
+        'description': '토마토계란볶음',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2017/06/30/465dd117e935b4d5daf1e34c520813c11.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6871978',
+        'ingredients': ['계란', '토마토', '다진파', '소금', '후춧가루', '설탕']
+    },
+    {
+        'title': '신선한 토마토 프리타타',
+        'description': '토마토프리타타',
+        'time': '',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/27/a98df5a1667ef544ec9896412e61cd5d1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/3974310',
+        'ingredients': ['계란', '그라나파다노 치즈', '우유', '양파', '토마토', '비엔나소시지', '브로콜리', '올리브오일', '후추', '소금', '다진마늘']
+    },
+    {
+        'title': '중화풍 토마토 계란 볶음',
+        'description': '토마토계란볶음',
+        'time': '30',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/06/08/6f5f4e59b9cd6a50787bcb0378409564.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6532908',
+        'ingredients': ['계란', '토마토', '대파', '다진마늘', '소금', '후춧가루', '식용유', '굴소스', '케찹']
+    },
+    {
+        'title': '토마토달걀볶음밥',
+        'description': '토마토계란볶음밥',
+        'time': '15',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/09/02/66d51d287a5d0d1ee0212f95a32b30e51.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6833242',
+        'ingredients': ['토마토', '달걀', '밥', '피망', '양파', '생크림', '집간장']
+    },
+   {
+        'title': '토마토소스 리조또 만들기 ~찬밥으로 만들어도 굿♪',
+        'description': '리조또',
+        'time': '',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/04/09/23763ba77116a89e02dee99f4efc9dea1.jpg',
+        'link': 'https://www.10000recipe.com/recipe/6556951',
+        'ingredients': ['토마토소스', '밥', '방울토마토', '양파', '냉동새우', '베이컨', '모짜렐라치즈', '올리브유', '마늘']
+    },
+    {
+        'title': '근사한 외식요리 따라잡기 토마토소스홍합찜',
+        'description': '토마토소스홍합찜',
+        'time': '60',
+        'image': 'https://recipe1.ezmember.co.kr/cache/recipe/2015/06/09/d65c4175ecfaaab690779ba36bd9c270.jpg',
+        'link': 'https://www.10000recipe.com/recipe/938207',
+        'ingredients': ['홍합', '토마토 중', '다진 양파', '페퍼론치노']
+    },
+
+
+
+
   // 더 많은 레시피 추가
 ];
